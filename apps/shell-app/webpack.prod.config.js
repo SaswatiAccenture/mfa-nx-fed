25708d6a4488629ae2c3e7b2bd089a36f18f7eeb
Choudhury <saswati.choudhury@accenture.com>
2024-01-25 23:28:16 +0530

    Pusing changes

--- apps/shell-app/webpack.prod.config.js
+++ apps/shell-app/webpack.prod.config.js
@@ -2,6 +2,15 @@ const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
+  remotes: [
+    ['header', 'https://saswatiaccenture.github.io/mfa-nx-fed/header'],
+    ['side-nav', 'https://saswatiaccenture.github.io/mfa-nx-fed/side-nav'],
+    [
+      'item-details',
+      'https://saswatiaccenture.github.io/mfa-nx-fed/item-details',
+    ],
+    ['footer', 'https://saswatiaccenture.github.io/mfa-nx-fed/footer'],
+  ],
   /*
    * Remote overrides for production.
    * Each entry is a pair of a unique name and the URL where it is deployed.
